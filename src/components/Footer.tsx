4be48d8fb9c8ff5a600990aa586fc443a4656aff
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2024-11-19 04:27:08 +0100

    make footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <footer className="flex justify-between items-center p-5 lg:px-20 lg:py-5 bg-red-600 text-white text-lg">
+    <footer className="flex justify-between items-center p-5 lg:px-20 lg:py-5 bg-red-600 text-white md:text-lg text-[10px]">
       <div>Copyright © 2024</div>
       <div></div>
       <div>DESIGN & DEVELOPMENT BY <span className="font-bold">ELO-OGHENE</span></div>
